b92abec711e0def657c0dbe7f1c8d13b585335f1
juerui <juerui@vip.qq.com>
2019-10-30 15:24:19 +0800

    首页-关于我们

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="g-box">
+		<!-- 头部 -->
 		<view class="g-hd">
 			<swiper class="m-banner" 
 				:indicator-dots="swiper.indicatorDots" 
@@ -19,6 +20,34 @@
 					<image src="../../static/img/job/ban-job.jpg" mode=""></image>
 				</swiper-item>
 			</swiper>
+		</view>
+		<!-- 内容区域 -->
+		<view class="g-bd">
+			<view class="m-bd">
+				<view class="title">
+					<view class="u-tit">
+						牛汽介绍
+					</view>
+					<view class="u-stit">
+						About Niuqi
+					</view>
+					<view class="u-line">
+						line
+					</view>
+				</view>
+				<view class="i-about">
+					<view class="img-box">
+						<image class="img-about" src="../../static/img/index/img-about.jpg"></image>
+					</view>
+					<view class="about-con">
+						浙江特浦科技有限公司成立于2016年，总部位于浙江省杭州市拱墅区，主要经营汽车保养套餐、常用件（事故件），拥有庞大的云数据库系统。牛汽是浙江特浦科技有限公司旗下一家互联网汽车后市场服务商，旨在结合移动互联网、 区块链、 大数据和人工智能等前沿技术，构建一个打通汽配供应链、 人才培训、 汽车保险、 汽车金融、 线上汽配APP和线下实体汽车养护中心为一体的新型车后服务平台，以全新的模式服务有车人！让汽配买卖变得更透明、便捷！
+					</view>
+					<view class="btn-more">
+						MORE +
+					</view>
+				</view>
+			</view>
+			
 		</view>
 	</view>
 </template>
