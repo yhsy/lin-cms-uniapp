7f7f9d54764e234ea9688598c50c80250697d036
fishku <juerui@vip.qq.com>
2019-11-04 20:57:14 +0800

    加盟-头部banner换成宣传视频

--- pages/join/join.vue
+++ pages/join/join.vue
@@ -1,9 +1,17 @@
 <template>
 	<view class="g-box">
 		<view class="g-hd">
-			<view class="m-banner">
-				<image src="../../static/img/join/ban-join.jpg" mode=""></image>
+			<view class="m-video">
+				<video 
+				src="https://niuqipei-video.oss-cn-hangzhou.aliyuncs.com/xuanchuan700.mp4" 
+				class="u-video"
+				controls></video>
 			</view>
+			<!-- 			
+				<view class="m-banner">
+					<image src="../../static/img/join/ban-join.jpg" mode=""></image>
+				</view>
+			-->
 		</view>
 		<view class="g-bd">
 			
