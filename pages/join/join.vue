27543c1160b5ef48eea77f97035684198ec41074
fishku <juerui@vip.qq.com>
2019-11-04 20:48:15 +0800

    加盟--头部和底部布局

--- pages/join/join.vue
+++ pages/join/join.vue
@@ -1,11 +1,24 @@
 <template>
-	<view>
-		招商加盟
+	<view class="g-box">
+		<view class="g-hd">
+			<view class="m-banner">
+				<image src="../../static/img/join/ban-join.jpg" mode=""></image>
+			</view>
+		</view>
+		<view class="g-bd">
+			
+		</view>
+		<i-footer></i-footer>
 	</view>
 </template>
 
 <script>
+	import iFooter from '../../components/footer/footer.vue'
+	
 	export default {
+		components: {
+			iFooter
+		},
 		data() {
 			return {
 				
@@ -15,5 +28,5 @@
 </script>
 
 <style lang="scss">
-
+@import './join.scss'
 </style>
